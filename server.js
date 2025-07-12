1dc3668dfca691464eeb4a86882a7b85898f7b72
mr-aiza <89704296+mr-aiza@users.noreply.github.com>
2025-07-12 16:56:09 +0330

    Update server.js

--- server.js
+++ server.js
@@ -47,6 +47,57 @@ async function uploadFile(filePath, contentBase64, message, sha = null) {
   return res.data;
 }
 
+// --- سرو کردن داینامیک sitemap.xml ---
+app.get('/sitemap.xml', (req, res) => {
+  res.header('Content-Type', 'application/xml');
+  res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  <url>
+    <loc>https://two24-96ud.onrender.com/</loc>
+    <lastmod>2025-07-12</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>https://two24-96ud.onrender.com/about.html</loc>
+    <lastmod>2025-07-12</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://two24-96ud.onrender.com/services.html</loc>
+    <lastmod>2025-07-12</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://two24-96ud.onrender.com/portfolio.html</loc>
+    <lastmod>2025-07-12</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+
+  <url>
+    <loc>https://two24-96ud.onrender.com/booking.html</loc>
+    <lastmod>2025-07-12</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+
+  <url>
+    <loc>https://two24-96ud.onrender.com/contact.html</loc>
+    <lastmod>2025-07-12</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+
+</urlset>`);
+});
+
 // --- رزرو مراسم ---
 app.post('/submit', async (req, res) => {
   try {
